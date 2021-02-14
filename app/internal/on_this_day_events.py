f766ff842cd2652bb8e097202f26ebf7aab8dabe
Gonny <gonnym@gmail.com>
2021-02-14 19:00:37 +0200

    fix: more files reviewed.
    This fix includes on_this_day_events.py

--- app/internal/on_this_day_events.py
+++ app/internal/on_this_day_events.py
@@ -14,11 +14,21 @@ from app.database.models import WikipediaEvents
 from app.dependencies import get_db
 
 
-def insert_on_this_day_data(
-        db: Session = Depends(get_db)
-) -> Dict[str, Any]:
+def insert_on_this_day_data(db: Session = Depends(get_db)) -> Dict[str, Any]:
+    """Fetches and inserts an "on this day" fact into the database.
+
+    Performs a network call to fetch the data.
+
+    Args:
+        db: Optional; The database connection.
+
+    Returns:
+        An "on this day" fact.
+
+    """
     now = datetime.now()
-    day, month = now.day, now.month
+    day = now.day
+    month = now.month
 
     res = requests.get(
         f'https://byabbe.se/on-this-day/{month}/{day}/events.json')
@@ -27,23 +37,36 @@ def insert_on_this_day_data(
     res_date = text.get('date')
     res_wiki = text.get('wikipedia')
     db.add(WikipediaEvents(events=res_events,
-                           date_=res_date, wikipedia=res_wiki))
+                           date_=res_date,
+                           wikipedia=res_wiki,
+                           ))
     db.commit()
     return text
 
 
-def get_on_this_day_events(
-        db: Session = Depends(get_db)
-) -> Dict[str, Any]:
+def get_on_this_day_events(db: Session = Depends(get_db)) -> Dict[str, Any]:
+    """Returns an "on this day" fact from the database.
+
+    Args:
+        db: Optional; The database connection.
+
+    Returns:
+        An "on this day" fact.
+
+    """
     try:
         data = (db.query(WikipediaEvents).
-                filter(
-            func.date(WikipediaEvents.date_inserted) == date.today()).
-                one())
+                filter(func.date(WikipediaEvents.date_inserted)
+                       == date.today())
+                .one()
+                )
 
     except NoResultFound:
         data = insert_on_this_day_data(db)
     except (SQLAlchemyError, AttributeError) as e:
         logger.error(f'on this day failed with error: {e}')
-        data = {'events': [], 'wikipedia': 'https://en.wikipedia.org/'}
+        data = {
+            'events': [],
+            'wikipedia': 'https://en.wikipedia.org/',
+        }
     return data
